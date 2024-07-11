aac83fa914ce65e731a448c46174aae7e2f5f16d
semsem-ethlas <102934232+semsem-ethlas@users.noreply.github.com>
2024-07-11 18:27:22 +0800

    u

---
+++ config.js
@@ -0,0 +1,3 @@
+export const openaiApiKey = "sk-proj-Kw9eRGttBxI4v5S8Bw6iT3BlbkF";
+export const etherscanApiKey = "ESSYV1XVW7Z33QM2DACRESJ1YN7JBS1BY4";
+export const slackWebhookUrl = "https://hooks.slack.com/services";
